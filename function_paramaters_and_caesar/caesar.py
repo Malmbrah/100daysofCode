9954539a894d8989f569114aaa210508a867f249
Malmbrah <Jonathanmalmm@gmail.com>
2024-12-01 14:21:17 +0100

    organiserte det bedre

--- function_paramaters_and_caesar/caesar.py
+++ function_paramaters_and_caesar/caesar.py
@@ -1,4 +1,4 @@
-import caesar_art
+import function_paramaters_and_caesar.caesar_art as caesar_art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
